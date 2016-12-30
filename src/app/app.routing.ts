17fea6932d76cd9f3edfd29fe76f60e8ccdc2105
aldoclg <aldoclg@gmail.com>
2016-12-30 13:32:25 -0200

    Rota com parametro

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,9 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+
 
 const APP_ROUTES: Routes = [
     { path: 'cursos', component: CursosComponent },
+    { path: 'curso/:id', component: CursoDetalheComponent },
     { path: 'login', component: LoginComponent },
     { path: '',      component: HomeComponent } 
 ];
