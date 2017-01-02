96b8cdec5cdb924d623a7d1d6e239be716eb482d
vntalgr <aldo.gracas@venturus.org.br>
2017-01-02 14:41:59 -0200

    extraindo queries

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from "rxjs/Rx";
 import { CursosService } from './cursos.service';
 
 @Component({
@@ -10,11 +11,29 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos: any[];
+  pagina: number;
+  inscricao: Subscription;
 
-  constructor(private cursoService: CursosService) { }
+  constructor(private cursoService: CursosService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.cursos = this.cursoService.getCursos();
+    this.inscricao = this.activatedRoute.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina'];
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina() {
+    this.router.navigate(['/cursos'], { queryParams: { 'pagina': ++this.pagina } });
   }
 
 }
